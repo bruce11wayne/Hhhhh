2044bfde44817690ad69c6dc89539322f55380ad
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-03-25 13:52:07 +0300

    Eye.js

--- plugins/Eye.js
+++ plugins/Eye.js
@@ -2,12 +2,12 @@ let timeout = 40000
 let poin = 5000
 let handler = async (m, { conn, command, usedPrefix }) => {
     conn.tokitoki = conn.tokitoki ? conn.tokitoki : {}
-    let id = m.chat
+    let id  = m.chat
     if (id in conn.tokitoki) {
         conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tokitoki[id][0])
         throw false
     }
-    let src = await (await fetch('https://raw.githubusercontent.com/kimos7/AURTER-BOT/main/Games/eyeanime.json')).json()
+    let src = await (await fetch('https://raw.githubusercontent.com/kimos71/AURTER-BOT/main/Games/eyeanime.json')).json()
   let json = src[Math.floor(Math.random() * src.length)]
     let caption = `*❰❖── ~『𝐿𝑈𝐹𝐹𝑌-𝐵𝛩𝑇』~──❖❱*\n *•┇❖↞استخدم انسحب للانسحاب┇👁️❯*
  *•┃❖↞الـوقـت⏳↞* *${(timeout / 1000).toFixed(2)}* *ثانية┇❯*
@@ -26,6 +26,6 @@ let handler = async (m, { conn, command, usedPrefix }) => {
 }
 handler.help = ['guesseye']
 handler.tags = ['game']
-handler.command = /^عين/i
+handler.command = /^عين$/i
 
 export default handler
