af24b808750f5909bf7736bb4ff08e45387914dd
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-03-30 20:56:52 +0300

    menus-menu.js

--- plugins/menus-menu.js
+++ plugins/menus-menu.js
@@ -19,7 +19,7 @@ let handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPr
     let uptime = clockString(_uptime)
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-let PictureUrl = 'https://telegra.ph/file/651e6e33b882fc53f09bd.mp4';
+let VideoUrl = 'https://telegra.ph/file/651e6e33b882fc53f09bd.mp4';
   let vn = './media/menu.mp3';
   const user = global.db.data.users[m.sender];
   const {money, joincount} = global.db.data.users[m.sender];
