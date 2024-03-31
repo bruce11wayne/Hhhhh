bc28dc5df19cff9359202b6106060dfdfcc61636
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-03-31 15:46:26 +0300

    تغيرالترحيب.js

--- plugins/تغيرالترحيب.js
+++ plugins/تغيرالترحيب.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text, isROwner, isOwner }) => {
   if (text) {
     global.db.data.chats[m.chat].sWelcome = text
     m.reply('*تــم وضــع رســالــة الــتـرحـيب !*')
-  } else throw `*أدخــل رســالــة الـتـرحــيب !*\n*عــشان تــعـمل مـنــشن أكــتب @user, عــشان تـحط أســم الـجروب أكـتب @group, عــشان تـحط وصــف أكــتب @desc*`
+  } else throw `*⦓          ⦓🦇⦔          ⦔*\n*❤️ꪶ→ @subject*\n*🍀ꪶ→ @user*\n*💌ꪶ→مـرحـبـاً بـك فـي عائلتك المتواضعه يا صديقي*\n*💝ꪶ→ اقـرا الوصف\n*⦓          ⦓🦇⦔          ⦔*\n المجموعه:*\n\n@desc\n\n*⦓          ⦓🦇⦔          ⦔*`
 }
 handler.help = ['setwelcome <text>']
 handler.tags = ['group']
