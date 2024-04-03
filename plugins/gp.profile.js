95284f9e04c40d08ff56ab803167b3db86f3c7ce
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-04-03 19:00:51 +0300

    gp.profile.js

--- plugins/gp.profile.js
+++ plugins/gp.profile.js
@@ -1,1 +1,73 @@
-const _0x243e77=_0x550e;(function(_0x13220a,_0x5eb18b){const _0x2454dd=_0x550e,_0x472b5f=_0x13220a();while(!![]){try{const _0x280d87=parseInt(_0x2454dd(0x117))/0x1*(-parseInt(_0x2454dd(0x103))/0x2)+parseInt(_0x2454dd(0x10e))/0x3*(parseInt(_0x2454dd(0x114))/0x4)+parseInt(_0x2454dd(0x102))/0x5+parseInt(_0x2454dd(0x11d))/0x6+parseInt(_0x2454dd(0x112))/0x7+parseInt(_0x2454dd(0x10d))/0x8*(parseInt(_0x2454dd(0x124))/0x9)+parseInt(_0x2454dd(0x128))/0xa*(-parseInt(_0x2454dd(0xfd))/0xb);if(_0x280d87===_0x5eb18b)break;else _0x472b5f['push'](_0x472b5f['shift']());}catch(_0x15c0b4){_0x472b5f['push'](_0x472b5f['shift']());}}}(_0x4313,0xafa3f));import{createHash}from'crypto';import _0x59ae48 from'awesome-phonenumber';import{canLevelUp,xpRange}from'../lib/levelling.js';function _0x4313(){const _0x42e2f9=['fromMe','update','2136105AatGNv','99692RuPbnY','profile','sender','quoted','command','hex','user','mentionedJid','split','profilePictureUrl','24TdTXJl','12bwhMwj','getNumber','catch','level','7758982xURGnq','@s.whatsapp.net','473384kqnPZU','\x0a\x20\x20\x20•\x20@','international','9ImqdHX','prems','\x0a└──────────────','./src/avatar_contact.png','getName','includes','1213638isICgF','\x0a⟣\x20*⚠️التحذيرات:*\x20','group','\x0a⟣\x20*📱الرقم:*\x20','users','\x0a\x20\x20\x20•\x20','\x0a┌───「\x20*بروفايل*\x20」\x0a⟣\x20*🔖\x20الإسم:*\x20\x0a\x20\x20\x20•\x20','10467GClMxI','data','jid','replace','6680jJJTGS','tags','\x0a⟣\x20*🎈Age*:\x20','perfil','\x20years','17237RsOjpK','digest','image'];_0x4313=function(){return _0x42e2f9;};return _0x4313();}function _0x550e(_0xed7f3c,_0x1d8934){const _0x4313ca=_0x4313();return _0x550e=function(_0x550e55,_0x346459){_0x550e55=_0x550e55-0xfc;let _0x32cb50=_0x4313ca[_0x550e55];return _0x32cb50;},_0x550e(_0xed7f3c,_0x1d8934);}let handler=async(_0x20a3bb,{conn:_0x500b1c,usedPrefix:_0x1f62cf,command:_0x1b8bf3})=>{const _0x4a3bbd=_0x550e;let _0x455bb0=_0x20a3bb[_0x4a3bbd(0x106)]?_0x20a3bb[_0x4a3bbd(0x106)][_0x4a3bbd(0x105)]:_0x20a3bb[_0x4a3bbd(0x10a)]&&_0x20a3bb[_0x4a3bbd(0x10a)][0x0]?_0x20a3bb[_0x4a3bbd(0x10a)][0x0]:_0x20a3bb[_0x4a3bbd(0x100)]?_0x500b1c[_0x4a3bbd(0x109)][_0x4a3bbd(0x126)]:_0x20a3bb[_0x4a3bbd(0x105)];if(!(_0x455bb0 in global['db'][_0x4a3bbd(0x125)]['users']))throw'✳️\x20The\x20user\x20is\x20not\x20found\x20in\x20my\x20database';let _0x400439=await _0x500b1c[_0x4a3bbd(0x10c)](_0x455bb0,_0x4a3bbd(0xff))[_0x4a3bbd(0x110)](_0x370cf5=>_0x4a3bbd(0x11a)),_0x3f79fd=global['db']['data'][_0x4a3bbd(0x121)][_0x455bb0],{name:_0x17167d,exp:_0x2b3b33,diamond:_0x28ad63,lastclaim:_0x2e06a5,registered:_0x3cf1c7,regTime:_0x671911,age:_0x4f0b4f,level:_0x41f3a7,role:_0x3b0f11,warn:_0x285224}=global['db']['data'][_0x4a3bbd(0x121)][_0x455bb0],{min:_0x35359f,xp:_0x336d0c,max:_0x4e3bd4}=xpRange(_0x3f79fd[_0x4a3bbd(0x111)],global['multiplier']),_0x1ad4a1=_0x500b1c[_0x4a3bbd(0x11b)](_0x455bb0),_0x2dbba1=_0x4e3bd4-_0x336d0c,_0x46cd40=global[_0x4a3bbd(0x118)][_0x4a3bbd(0x11c)](_0x455bb0[_0x4a3bbd(0x10b)]`@`[0x0]),_0x266652=createHash('md5')[_0x4a3bbd(0x101)](_0x455bb0)[_0x4a3bbd(0xfe)](_0x4a3bbd(0x108)),_0x276824=_0x4a3bbd(0x123)+_0x1ad4a1+'\x20'+(_0x3cf1c7?_0x4a3bbd(0x122)+_0x17167d+'\x20':'')+_0x4a3bbd(0x115)+_0x455bb0[_0x4a3bbd(0x127)](/@.+/,'')+_0x4a3bbd(0x120)+_0x59ae48('+'+_0x455bb0['replace'](_0x4a3bbd(0x113),''))[_0x4a3bbd(0x10f)](_0x4a3bbd(0x116))+'\x0a⟣\x20*🔗الرابط:*\x20wa.me/'+_0x455bb0[_0x4a3bbd(0x10b)]`@`[0x0]+(_0x3cf1c7?_0x4a3bbd(0x12a)+_0x4f0b4f+_0x4a3bbd(0xfc):'')+_0x4a3bbd(0x11e)+_0x285224+'/'+maxwarn+_0x4a3bbd(0x119);_0x500b1c['sendFile'](_0x20a3bb['chat'],_0x400439,'perfil.jpg',_0x276824,_0x20a3bb,![],{'mentions':[_0x455bb0]}),_0x20a3bb['react'](done);};handler['help']=[_0x243e77(0x12b)],handler[_0x243e77(0x129)]=[_0x243e77(0x11f)],handler[_0x243e77(0x107)]=[_0x243e77(0x104),_0x243e77(0x12b),'بروفايل'];export default handler;
+cmd({
+            pattern: "profile",
+            alias: ["ايدي","بروفايل","me","انا","ا"],
+            desc: "Shows profile of user.",
+            category: "group",
+            filename: __filename,
+        },
+        async(Void, citel, text) => {
+            var bio = await Void.fetchStatus(citel.sender);
+            var bioo = bio.status;
+            let meh = citel.sender;
+            const userq = await Levels.fetch(citel.sender, "RandomXP");
+            const lvpoints = userq.level;
+            var role = "فنان✨";
+             if (lvpoints <=  2) { var role = "🏳مواطن"; } 
+	else if (lvpoints <=  4) { var role = "👼طبيب اطفال"; } 
+	else if (lvpoints <=  6) { var role = "🧙‍♀️ساحر";  } 
+	else if (lvpoints <=  8) { var role = "🧙‍♂️معالج روحاني"; }
+	else if (lvpoints <= 10) { var role = "🧚🏻طفل ملاك";  } 
+	else if (lvpoints <= 12) { var role = "🧜ملاك"; } 
+	else if (lvpoints <= 14) { var role = "🧜‍♂️سيد الملاك";} 
+	else if (lvpoints <= 16) { var role = "🌬طفل نوبل"; } 
+	else if (lvpoints <= 18) { var role = "❄نوبل"; }
+	else if (lvpoints <= 20) { var role = "⚡سريع النخبه"; } 
+	else if (lvpoints <= 22) { var role = "🎭نخبه"; } 
+	else if (lvpoints <= 24) { var role = "🥇بارع I"; }
+	else if (lvpoints <= 26) { var role = "🥈بارع II"; } 
+	else if (lvpoints <= 28) { var role = "🥉متفوق بارع"; }
+	else if (lvpoints <= 30) { var role = "🎖متفوق مسيطر";} 
+	else if (lvpoints <= 32) { var role = "🏅متفوق النخبه"; }
+	else if (lvpoints <= 34) { var role = "🏆فائق";}
+	else if (lvpoints <= 36) { var role = "💍فائق I";}
+	else if (lvpoints <= 38) { var role = "💎فائق Ii";} 
+	else if (lvpoints <= 40) { var role = "🔮سيد اللعبه";} 
+	else if (lvpoints <= 42) { var role = "🛡اسطوره III";} 
+	else if (lvpoints <= 44) { var role = "🏹اسطوره II";} 
+	else if (lvpoints <= 46) { var role = "⚔اسطوره"; } 
+	else if (lvpoints <= 55) { var role = "🐉ابدي"; }
+	
+            let ttms = `${userq.xp}` / 8;
+            const timenow = moment(moment())
+                .format('HH:mm:ss')
+            moment.tz.setDefault('Africa/Lagos')
+                .locale('id')
+            try {
+                pfp = await Void.profilePictureUrl(citel.sender, "image");
+            } catch (e) {
+                pfp = await botpic();
+            }
+            const profile = `
+*↫ صوره قمر زي صاحبها 🥺♥.!*
+*⌁︙اسمڪ🪪↫ ${citel.pushName}*
+*⌁︙تفاعلـڪ💥↫ سايق مخده 😹*
+*⌁︙مستواڪ💎↫ ${userq.level}*
+*⌁︙دورڪ🏅↫ ${role}*
+*⌁︙نقاطـڪ♦️↫ ${userq.xp}*
+*⌁︙رسائلـڪ🧩↫ ${ttms}*
+*⌁︙البـايـــو⚡↫ ${bioo}*
+`;
+            let buttonMessage = {
+                image: {
+                    url: pfp,
+                },
+                caption: profile,
+                footer: tlang().footer,
+                headerType: 4,
+            };
+            Void.sendMessage(citel.chat, buttonMessage, {
+                quoted: citel,
+            });
+
+        }
+    )
