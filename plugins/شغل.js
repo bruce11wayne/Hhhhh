37ba4bc5ed3db5ea0743b7e9a9bf310cc79d9002
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-04-10 19:12:06 +0300

    Song.js

--- plugins/شغل.js
+++ plugins/شغل.js
@@ -78,7 +78,7 @@ var handler = async (m, { conn, command, text, usedPrefix }) => {
 
 handler.help = ['play'].map((v) => v + ' <query>');
 handler.tags = ['downloader'];
-handler.command = /^شغل$/i;
+handler.command = /^اغنيه|صوت$/i;
 
 handler.exp = 0;
 handler.diamond = false;
