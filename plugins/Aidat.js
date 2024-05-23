1cae5fef80b3bd9c1695e6a840a289f6adb8c7fe
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-05-23 20:27:01 +0300

    Aidat.js

--- plugins/Aidat.js
+++ plugins/Aidat.js
@@ -5,6 +5,10 @@ const dir = [
 'https://telegra.ph/file/faa51649103c4c3be501e.mp4',
 'https://telegra.ph/file/b5a065eeaa159e8e75711.mp4',
 'https://telegra.ph/file/7a7e52444e24ada470233.mp4',
+'https://telegra.ph/file/9c6f798fa46af64d9c032.mp4',
+'https://telegra.ph/file/98fb9fa0e26feb008186e.mp4',
+'https://telegra.ph/file/0932954e91cf5e54de300.mp4',
+'https://telegra.ph/file/fdb0281dd63f2b979bdf4.mp4',
 ];
 let handler = async (m, { conn }) => {
   conn.sendFile(m.chat, dir[Math.floor(Math.random() * dir.length)], 'dado.webp', '', m)
