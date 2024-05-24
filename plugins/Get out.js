17fd6ce2e618d862193f4999410a2a85cad62fd4
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-05-24 20:27:40 +0300

    Delete plugins/Get out.js

--- plugins/Get out.js
+++
@@ -1,9 +0,0 @@
-const handler = async (m, {conn, text, command}) => {
-  const id = text ? text : m.chat;
-  await conn.reply(id, '*سمعاً وطاعة سيدي بروس!! (≧ω≦)ゞ*');
-  await conn.groupLeave(id);
-};
-handler.command = /^(دزها)$/i;
-handler.group = true;
-handler.rowner = true;
-export default handler;
