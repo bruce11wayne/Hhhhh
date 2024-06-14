ebca2e37e3a055c6d8b4bee5ea024db1a2771cdd
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-06-14 06:59:38 +0300

    User attended.js

---
+++ plugins/User attended.js
@@ -0,0 +1,13 @@
+export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
+  if (m.isBaileys && m.fromMe) return !0;
+  if (m.isGroup) return !1;
+  if (!m.message) return !0;
+  if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0;
+  const chat = global.db.data.chats[m.chat];
+  const bot = global.db.data.settings[this.user.jid] || {};
+  if (bot.antiPrivate && !isOwner && !isROwner) {
+    await m.reply(`*[❗] مرحبا @${m.sender.split`@`[0]}, تم حضرك من البوت عشان الكلام خاص ممنوع تواصل مع المطور اكتب المطور وهو يفكك🎃*`, false, {mentions: [m.sender]});
+    await this.updateBlockStatus(m.chat, 'block');
+  }
+  return !1;
+}
