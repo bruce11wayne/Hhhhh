fa4302ac6fa8f6ff3c27c7854eae83d0b0e9678d
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-08-19 19:46:53 +0300

    رقم التسلسل.js

--- plugins/رقم التسلسل.js
+++ plugins/رقم التسلسل.js
@@ -4,12 +4,12 @@ let handler = async function (m, { conn, text, usedPrefix }) {
   let sn = createHash('md5').update(m.sender).digest('hex')
   m.reply(
     `
-▢ *seriel number* : ${sn}
+▢ *الرقم التسلسلي التابع إليك* : ${sn}
 `.trim()
   )
 }
 handler.help = ['mysn']
 handler.tags = ['rg']
-handler.command = ['nserie', 'sn', 'mysn']
+handler.command = ['الرقم التسلسلي']
 handler.register = true
 export default handler
