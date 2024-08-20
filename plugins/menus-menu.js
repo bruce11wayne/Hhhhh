e458c80a41159fd9937602e7acdbc074dab6eaf0
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-08-20 14:51:33 +0300

    menus-menu.js

--- plugins/menus-menu.js
+++ plugins/menus-menu.js
@@ -129,7 +129,7 @@ conn.sendMessage(m.chat, {
 }; 
 handler.help = ['main']
 handler.tags = ['group']
-handler.command = ['Menu'] 
+handler.command = ['قروبات'] 
 
 export default handler
 function clockString(ms) {
