7e45270510b9eda16a33b65bcd84ca1208a5c6b3
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-08-20 22:20:10 +0300

    الصلاة على النبي صلى الله عليه وسلم.js

--- plugins/الصلاة على النبي صلى الله عليه وسلم.js
+++ plugins/الصلاة على النبي صلى الله عليه وسلم.js
@@ -1,1 +1,26 @@
-.
+
+const dir = [
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'', 
+];
+let handler = async (m, { conn }) => {
+  conn.sendFile(m.chat, dir[Math.floor(Math.random() * dir.length)], 'dado.webp', '', m)
+}
+handler.help = ['dado']
+handler.tags = ['game']
+handler.command = ['الصلاة'] 
+
+export default handler
