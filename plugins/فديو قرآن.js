b979ca74d2052e95a4c7364e35e06eb30c7d581c
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-08-20 13:45:55 +0300

    فديو قرآن.js

--- plugins/فديو قرآن.js
+++ plugins/فديو قرآن.js
@@ -10,9 +10,16 @@ const dir = [
 'https://telegra.ph/file/df4fe3e593bf19078c486.mp4',
 'https://telegra.ph/file/127cddb24e4573bbb92aa.mp4',
 'https://telegra.ph/file/cdd34e7143c7aad75fbde.mp4',
-'',
-'',
-'',
+'https://telegra.ph/file/04c48161c3e05e7ba9df1.mp4',
+'https://telegra.ph/file/3499fdb9779b5a7f15bf3.mp4',
+'https://telegra.ph/file/6240b4e14f8a3d730e0d5.mp4',
+'https://telegra.ph/file/dbbb685f1d2a647b6cf9a.mp4',
+'https://telegra.ph/file/b9706fb6a0533a360ef96.mp4',
+'https://telegra.ph/file/2163f64e99a9ddddf6b2d.mp4',
+'https://telegra.ph/file/2ed832b40b1fc08106c9b.mp4',
+'https://telegra.ph/file/ca494e4816708f720545a.mp4',
+'https://telegra.ph/file/ff0fccab368b08d360fac.mp4',
+'https://telegra.ph/file/66dc43e939cf744854445.mp4',
 ];
 let handler = async (m, { conn }) => {
   conn.sendFile(m.chat, dir[Math.floor(Math.random() * dir.length)], 'dado.webp', '', m)
