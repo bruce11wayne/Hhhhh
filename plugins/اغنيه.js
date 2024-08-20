c55bf471bd915e5b331a9769d9c22443c72b9a52
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-08-20 18:07:56 +0300

    Add files via upload

--- plugins/اغنيه.js
+++ plugins/اغنيه.js
@@ -1,34 +1,129 @@
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper';
-
-let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
-  if (!args || !args[0]) throw `*مــثال :\n${usedPrefix + command} https://youtu.be/YzkTFFwxtXI*`;
-  if (!args[0].match(/youtu/gi)) throw `❎ تـأكـد أنـه رابـط يــوتيـوب !`;
-
-
+import fetch from 'node-fetch';
+import yts from 'yt-search';
+import ytdl from 'ytdl-core';
+import axios from 'axios';
+const handler = async (m, {command, usedPrefix, conn, text}) => {
+  if (!text) throw `*[❗]هاذا الامر خاص  بي تنزيل الفيديوهات او الاغاني عن طريق الاسم او الرابط تقدر تجيب الرابط من اوامر البحث في البوت*\n *~احب افكرك انا خالي المسؤوليه من ذنوب اغانيك او ما تشاهده~*\n\n*—◉ مـثـال:*\n*${usedPrefix + command} save your ters*`;
   try {
-    let q = '128kbps'; 
-    let v = args[0]; 
-    const yt = await youtubedl(v).catch(async () => await youtubedlv2(v)); 
-    const dl_url = await yt.audio[q].download(); 
-    const title = await yt.title; 
-
-    conn.sendFile(
-      m.chat,
-      dl_url,
-      title + '.mp3',
-      null, 
-      m,
-      false,
-      { mimetype: 'audio/mpeg' }
-    );
-
+    if (command == 'اغنيه') {
+      conn.reply(m.chat, `*_⏳ جاري التحميل⌛_*`, m);
+      try {
+        const mediaa = await ytPlay(text);
+        const audiocore = mediaa.result2?.[0]?.audio || mediaa.result2?.[1]?.audio || mediaa.result2?.[2]?.audio || null;
+        const aa = await conn.sendMessage(m.chat, {audio: {url: audiocore}, fileName: `error.mp3`, mimetype: 'audio/mp4'}, {quoted: m});
+        if (!aa) {
+        throw new Error('*[❗] El primero metodo fallo, intentando otro...*');
+       }        
+      } catch {
+        const res = await fetch(`https://api.akuari.my.id/downloader/youtube?link=${yt_play[0].url}`);
+        const json = await res.json();
+        const aa_1 = await conn.sendMessage(m.chat, {audio: {url: json.result.audio}, fileName: `error.mp3`, mimetype: 'audio/mp4'}, {quoted: m});
+        if (!aa_1) aa_1 = await conn.sendFile(m.chat, json.result.audio, 'error.mp3', null, m, false, {mimetype: 'audio/mp4'});
+      }
+    }
+    if (command == 'فيديو') {
+      conn.reply(m.chat, `*_⌛جاري التحميل⏳_*`, m);
+      try {
+        const mediaa = await ytPlayVid(text);
+        const aa_2 = await conn.sendMessage(m.chat, {video: {url: mediaa.result}, fileName: `error.mp4`, caption: `*『ITACHI♦️BOT』\n انت مسؤل عن ذنوب اغانيك*`, thumbnail: mediaa.thumb, mimetype: 'video/mp4'}, {quoted: m});
+        if (!aa_2) {
+        throw new Error('*[❗] El primero metodo fallo, intentando otro...*');
+       }
+      } catch {
+        const res = await fetch(`https://api.lolhuman.xyz/api/ytplay?apikey=${lolkeysapi}&query=${yt_play[0].title}`);
+        const json = await res.json();
+        await conn.sendFile(m.chat, json.result.video, 'error.mp4', `*『🐉┇𝙼𝙴𝙳𝙾 𝙱𝙾𝚃』\n انا لا اتحمل ذنوب اغانيك او ما تشاهده*`, m);
+      }
+    }
   } catch {
-    await m.reply(`❎ لـم اسـتطيع تـحمـيل الـملـف !`)
+    throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*';
   }
 };
+handler.help = ['play.1', 'play.2'].map((v) => v + ' <texto>');
+handler.tags = ['downloader'];
+handler.command = ['اغنيه', 'فيديو'];
+export default handler;
 
-handler.help = ['ytmp3 <url>']
-handler.tags = ['dl']
-handler.command = ['اغنيه', 'اغنية'] 
+function bytesToSize(bytes) {
+  return new Promise((resolve, reject) => {
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes === 0) return 'n/a';
+    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
+    if (i === 0) resolve(`${bytes} ${sizes[i]}`);
+    resolve(`${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]}`);
+  });
+}
 
-export default handler
+async function ytMp3(url) {
+  return new Promise((resolve, reject) => {
+    ytdl.getInfo(url).then(async (getUrl) => {
+      const result = [];
+      for (let i = 0; i < getUrl.formats.length; i++) {
+        const item = getUrl.formats[i];
+        if (item.mimeType == 'audio/webm; codecs=\"opus\"') {
+          const {contentLength} = item;
+          const bytes = await bytesToSize(contentLength);
+          result[i] = {audio: item.url, size: bytes};
+        }
+      }
+      const resultFix = result.filter((x) => x.audio != undefined && x.size != undefined);
+      const tiny = await axios.get(`https://tinyurl.com/api-create.php?url=${resultFix[0].audio}`);
+      const tinyUrl = tiny.data;
+      const title = getUrl.videoDetails.title;
+      const thumb = getUrl.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url;
+      resolve({title, result: tinyUrl, result2: resultFix, thumb});
+    }).catch(reject);
+  });
+}
+
+async function ytMp4(url) {
+  return new Promise(async (resolve, reject) => {
+    ytdl.getInfo(url).then(async (getUrl) => {
+      const result = [];
+      for (let i = 0; i < getUrl.formats.length; i++) {
+        const item = getUrl.formats[i];
+        if (item.container == 'mp4' && item.hasVideo == true && item.hasAudio == true) {
+          const {qualityLabel, contentLength} = item;
+          const bytes = await bytesToSize(contentLength);
+          result[i] = {video: item.url, quality: qualityLabel, size: bytes};
+        }
+      }
+      const resultFix = result.filter((x) => x.video != undefined && x.size != undefined && x.quality != undefined);
+      const tiny = await axios.get(`https://tinyurl.com/api-create.php?url=${resultFix[0].video}`);
+      const tinyUrl = tiny.data;
+      const title = getUrl.videoDetails.title;
+      const thumb = getUrl.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url;
+      resolve({title, result: tinyUrl, rersult2: resultFix[0].video, thumb});
+    }).catch(reject);
+  });
+}
+
+async function ytPlay(query) {
+  return new Promise((resolve, reject) => {
+    yts(query).then(async (getData) => {
+      const result = getData.videos.slice( 0, 5 );
+      const url = [];
+      for (let i = 0; i < result.length; i++) {
+        url.push(result[i].url);
+      }
+      const random = url[0];
+      const getAudio = await ytMp3(random);
+      resolve(getAudio);
+    }).catch(reject);
+  });
+}
+
+async function ytPlayVid(query) {
+  return new Promise((resolve, reject) => {
+    yts(query).then(async (getData) => {
+      const result = getData.videos.slice( 0, 5 );
+      const url = [];
+      for (let i = 0; i < result.length; i++) {
+        url.push(result[i].url);
+      }
+      const random = url[0];
+      const getVideo = await ytMp4(random);
+      resolve(getVideo);
+    }).catch(reject);
+  });
+                                                   }
