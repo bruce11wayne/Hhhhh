bc99c9403453ed1d11a919204c0a09593aad1096
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-08-20 13:57:08 +0300

    نصيحه.js

---
+++ plugins/نصيحه.js
@@ -0,0 +1,31 @@
+const dir = [
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+'',
+];
+let handler = async (m, { conn }) => {
+  conn.sendFile(m.chat, dir[Math.floor(Math.random() * dir.length)], 'dado.webp', '', m)
+}
+handler.help = ['dado']
+handler.tags = ['game']
+handler.command = ['نصيحه'] 
+
+export default handler
